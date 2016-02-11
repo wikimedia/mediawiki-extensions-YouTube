b999f4225ff74b8cef8909d797883ea6bee73aa3
man01 <1023qaz@naver.com>
2016-02-11 12:37:12 +0900

    Update YouTube.class.php

--- YouTube.class.php
+++ YouTube.class.php
@@ -496,5 +496,117 @@ class YouTube {
 			return "<script type=\"text/javascript\" src=\"{$url}\"></script>";
 		}
 	}
+	
+	
+	function embedYouTube_url2dpid( $url ) {
+		$id = $url;
+		
+		if( preg_match( '/^(?:http|https|)(?::\/\/|)(?:videofarm|tvpot)\.daum\.net\/(?:.*?(?:vid=|v\/))?([0-9A-Za-z%$]+)/', $url, $preg ) ) {
+			$id = $preg[1];
+		}
+		preg_match( '/([0-9A-Za-z%$]+)/', $id, $preg );
+		$id = $preg[1];
+		return $id;
+	}
+	function embedDaumPot( $input, $argv, $parser ) {
+		$dpid = '';
+		$width = $max_width = 640;
+		$height = $max_height = 360;
+	
+		if ( !empty( $argv['dpid'] ) ) {
+			$dpid = embedYouTube_url2dpid( $argv['dpid'] );
+		} elseif ( !empty( $input ) ) {
+			$dpid = embedYouTube_url2dpid( $input );
+		}
+	
+		if ( !empty( $argv['height'] ) ) {
+			$argv['height'] = str_replace( 'px', '', $argv['height'] );
+			if ( $argv['height'] <= $max_height ) {
+				$height = $argv['height'];
+			}
+		}
+		if ( !empty( $argv['width'] ) ) {
+			$argv['width'] = str_replace( 'px', '', $argv['width'] );
+			if ( $argv['width'] <= $max_width ) {
+				$width = $argv['width'];
+		}
+	}
+	
+	if ( !empty( $dpid ) ) {
+		return "<iframe width='{$width}' height='{$height}' src='//videofarm.daum.net/controller/video/viewer/Video.html?vid={$dpid}&play_loc=undefined&alert=true' frameborder='0' scrolling='no' ></iframe>";
+		}
+	}
+function embedYouTube_url2dmid( $url ) {
+	$id = $url;
+	if( preg_match( '/^(?:http|https|)(?::\/\/|)(?:www\.)dailymotion\.com\/(?:embed\/)?video\/([0-9a-z]+).*$/', $url, $preg ) ) {
+		$id = $preg[1];
+	} elseif ( preg_match( '/^(?:http|https|)(?::\/\/|)dai\.ly\/([0-9a-z]+)$', $url, $preg ) ) {
+		$id = $preg[1];
+	}
+	preg_match( '/([0-9a-z]+)/', $id, $preg );
+	$id = $preg[1];
+	return $id;
+}
+function embedDailyMotion( $input, $argv, $parser ) {
+	$dmid = '';
+	$width = $max_width = 560;
+	$height = $max_height = 315;
+	
+	if ( !empty( $argv['dmid'] ) ) {
+		$dmid = embedYouTube_url2dmid( $argv['dmid'] );
+	} elseif ( !empty( $input ) ) {
+		$dmid = embedYouTube_url2dmid( $input );
+	}
+	
+	if ( !empty( $argv['height'] ) ) {
+		$argv['height'] = str_replace( 'px', '', $argv['height'] );
+		if ( $argv['height'] <= $max_height ) {
+			$height = $argv['height'];
+		}
+	}
+	if ( !empty( $argv['width'] ) ) {
+		$argv['width'] = str_replace( 'px', '', $argv['width'] );
+		if ( $argv['width'] <= $max_width ) {
+			$width = $argv['width'];
+		}
+	}
+	
+	return "<iframe frameborder='0' width='{$width}' height='{$height}' src='//www.dailymotion.com/embed/video/{$dmid}' allowfullscreen></iframe>";
+}
+function embedYouTube_url2vmid( $url ) {
+	$id = $url;
+	if( preg_match( '/^(?:http|https|)(?::\/\/|)(?:player\.)vimeo\.com\/(?:video\/|[a-z\/]+|)([0-9]+)$/', $url, $preg ) ) {
+		$id = $preg[1];
+	}
+	preg_match( '/([0-9]+)/', $id, $preg );
+	$id = $preg[1];
+	return $id;
+}
+function embedVimeo( $input, $argv, $parser ) {
+	$vmid = '';
+	$width = $max_width = 640;
+	$height = $max_height = 360;
+	
+	if ( !empty( $argv['vmid'] ) ) {
+		$vmid = embedYouTube_url2vmid( $argv['vmid'] );
+	} elseif ( !empty( $input ) ) {
+		$vmid = embedYouTube_url2vmid( $input );
+	}
+	
+	if ( !empty( $argv['height'] ) ) {
+		$argv['height'] = str_replace( 'px', '', $argv['height'] );
+		if ( $argv['height'] <= $max_height ) {
+			$height = $argv['height'];
+		}
+	}
+	if ( !empty( $argv['width'] ) ) {
+		$argv['width'] = str_replace( 'px', '', $argv['width'] );
+		if ( $argv['width'] <= $max_width ) {
+			$width = $argv['width'];
+		}
+	}
+	
+	return "<iframe frameborder='0' width='{$width}' height='{$height}' src='https://player.vimeo.com/video/{$vmid}' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+}
 
 }
